917b5e4e0a143193f24c53c233cb243d3e814fa1
Mohamed  Sayed Abdelmaksoud <62207287+Soda22@users.noreply.github.com>
2020-07-28 13:56:23 +0200

    Initial Commit

---
+++ الجامع/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  الجامع
+//
+//  Created by Mohamed on 7/28/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
